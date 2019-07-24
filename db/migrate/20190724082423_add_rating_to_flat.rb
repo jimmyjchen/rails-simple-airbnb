f241e26ad992e50848c55136d81ab233fe389e75
Jimmy Chen <jimmyjchen@outlook.com>
2019-07-24 17:32:11 +0800

    Simple Airbnb finished banner, card component 80% complete

---
+++ db/migrate/20190724082423_add_rating_to_flat.rb
@@ -0,0 +1,5 @@
+class AddRatingToFlat < ActiveRecord::Migration[5.2]
+  def change
+    add_column :flats, :rating, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_24_070523) do
+ActiveRecord::Schema.define(version: 2019_07_24_082423) do
 
   create_table "flats", force: :cascade do |t|
     t.string "name"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 2019_07_24_070523) do
     t.integer "number_of_guests"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_url"
+    t.integer "rating"
   end
 
 end
