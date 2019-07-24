b6b97dac9e02509eb53e89f1f8059edc665d6811
Jimmy Chen <jimmyjchen@outlook.com>
2019-07-24 15:26:18 +0800

    frontend install

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,39 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'Shinjuku! Free Wi-Fi + Good Location + Clean Room!',
+  address: '7-1, Nishi Shinjuku 3-chome, Shibuya-ku, Tokyo 163-1017',
+  description: 'This is Cozy and Clean Mordern Room in Shinjuku Area. It is steps from the best spots to Chill / Eat / Drink or walk around.',
+  price_per_night: 60,
+  number_of_guests: 4
+)
+
+Flat.create!(
+  name: 'Private Pool House with Amazing Views!',
+  address: '972 Rainbow Road Los Angeles CA 90017',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory.',
+  price_per_night: 150,
+  number_of_guests: 4
+)
+
+Flat.create!(
+  name: 'Explore Old Barcelona from a Loft-Style Studio',
+  address: 'Comandante Izarduy 67 Cornellà de Llobregat Barcelona 08940',
+  description: 'In a charming neighborhood with wonderful restaurants and shops, and sights within walking distance, as well as the city center, Plaza Catalunya about 5 minutes.',
+  price_per_night: 190,
+  number_of_guests: 2
+)
+
+Flat.create!(
+  name: 'Champs-Élysées Golden triangle flat with mezzanine',
+  address: 'Comandante Izarduy 67 Cornellà de Llobregat Barcelona 08940',
+  description: 'Newly renovated little apartment with mezzanine, compact and design,well-equipped, direct elevator.',
+  price_per_night: 65,
+  number_of_guests: 2
+)
